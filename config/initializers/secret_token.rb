f66acd8ce807a6a14096c8fa357e60110cc9b97d
Itay Klein <klein.itay@gmail.com>
2013-04-14 16:20:39 +0300

    start

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Contactim::Application.config.secret_token = '4d0802b5184ffe33c6dad70880a41f013f6c48534ac858a64c451889ddf5bfe8751beb4e698bc0051cf668bc07af3ffedfe46bf68fb1eaf15235cd890c226248'
